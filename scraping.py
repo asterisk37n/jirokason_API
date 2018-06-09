f5f850d127276beae3fe9b286b29899e846b37fd
WATANABE NaokI <asterisk37n@gmail.com>
2018-06-09 18:21:51 +0900

    revert

--- scraping.py
+++ scraping.py
@@ -1,9 +1,8 @@
 import requests
 import time
-from bs4 import BeautifulSoup, NavigableString
+from bs4 import BeautifulSoup
 import time
 import json
-import re
 
 top_url = 'http://xn--4dkp5a8a4562a1c2fvhm.com/shoplist'
 
@@ -23,49 +22,7 @@ def convert_address(address_text):
         address = address_text
     return zipcode, address
 
-def convert_open_hours(element):
-    childa = '～'
-    hours = {}
-    week_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    # Initialize hours
-    for d in week_days:
-        hours[d] = []
-
-    for duration in element.childGenerator():
-        if duration.name == 'br':
-            continue
-
-        pattern_childa = r'\[.' + childa + '.\]'
-        pattern_dot = r'\[.・.\]'
-        pattern_hour = r'\d+:\d+' + childa + '翌?\d+:\d+'
-        
-        duration = duration.string
-        matched_childa = re.findall(pattern_childa, duration)
-        matched_dot = re.findall(pattern_dot, duration)
-        matched_hour = re.findall(pattern_hour, duration)
-        if not matched_hour:
-            continue
-        print(matched_childa)
-        print(matched_dot)
-        print(matched_hour)
-        
-        # if working hours are the same accross days
-        if (not matched_childa) and (not matched_dot):
-            matched_hour = matched_hour[0]
-            start_time, finish_time = matched_hour.split(childa)
-            value = {'start': start_time, 'finish': finish_time}
-            for d in week_days:
-                hours[d].append(value)
-
-        #if matched_childa:
-        #    days = list('月火水木金土日')
-        #    start_day, finish_day = matched_childa[0][1:-1].split(childa)
-        #    print(start_day, finish_day)
-        #    working_days = days[days.index(start_day), days.index(finish_day)]
-        #    print(working_days)
-    return hours
-
-for i, row in enumerate(restaurant_links):
+for row in restaurant_links:
     url = row['url']
     res = requests.get(url)
     soup = BeautifulSoup(res.content, 'lxml')
@@ -73,19 +30,17 @@ for i, row in enumerate(restaurant_links):
     name = tds[1].text.strip()
     zipcode, address = convert_address(tds[3].text)
     tel = tds[5].text
-    print(name)
-    print(tds[7])
-    opens = convert_open_hours(tds[7])
-    print(opens)
+    opens = tds[7].text
     holidays = tds[9].text
     access = tds[11].text
     opened_in = tds[13].text
     menu = tds[15].text
+
     # get coordination
     soup_geo = BeautifulSoup(requests.get('http://www.geocoding.jp/api/?v=1.1&q='+address).content, 'xml')
+    print(float(soup_geo.lat.text))
 
     data.append({
-        'id': i,
         'name': name,
         'zipcode': zipcode,
         'address': address.strip(),
@@ -97,6 +52,8 @@ for i, row in enumerate(restaurant_links):
         'menu': menu,
         'coordinate': {'lat': float(soup_geo.lat.text), 'lng': float(soup_geo.lng.text)}
         })
+    print(data)
+    time.sleep(3)
 else:
     jsondata = {'restaurants': data}
 
